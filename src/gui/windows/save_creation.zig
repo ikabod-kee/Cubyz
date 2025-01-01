caf0ea3fdbe3c9f45bb8ea4dcaec7bf027e76452
ikabod-kee <redurestract@gmail.com>
2024-12-31 22:05:48 -0500

    Changed default world parameters, other changes
    I changed the ocean's radius so that it blends better with land, but it causes a lot of lag.

--- src/gui/windows/save_creation.zig
+++ src/gui/windows/save_creation.zig
@@ -51,10 +51,10 @@ fn flawedCreateWorld() !void {
 		mapGenerator.put("id", "cubyz:mapgen_v1"); // TODO: Make this configurable
 		generatorSettings.put("mapGenerator", mapGenerator);
 		const climateWavelengths = main.ZonElement.initObject(main.stackAllocator);
-		climateWavelengths.put("hot_cold", 1600);
-		climateWavelengths.put("land_ocean", 1600);
-		climateWavelengths.put("wet_dry", 1600);
-		climateWavelengths.put("mountain", 64);
+		climateWavelengths.put("hot_cold", 2400);
+		climateWavelengths.put("land_ocean", 3200);
+		climateWavelengths.put("wet_dry", 2400);
+		climateWavelengths.put("mountain", 500);
 		generatorSettings.put("climateWavelengths", climateWavelengths);
 		try main.files.writeZon(generatorSettingsPath, generatorSettings);
 	}
